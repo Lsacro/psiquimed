2ed0b345291698ebecb74772bfee51a6b6544c3e
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2025-04-15 18:02:59 -0500

    PRivacy page and github actions duplicated deleted

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import PrivacyPage from "./pages/PrivacyPage";
 
 import "./app.css";
 
@@ -9,7 +8,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
       </Routes>
     </>
   );
